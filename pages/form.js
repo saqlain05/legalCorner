27c85fdaf89e256576589ed259d19c3cd64dd81b
saqlain05 <roseisred9821@gmail.com>
2020-11-25 14:32:22 +0530

    service

--- pages/form.js
+++ pages/form.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Editor from '../components/backend/Editor'
+// import Editor from '../components/backend/Editor'
 import Form from '../components/backend/Form'
 import Header from '../components/backend/Header'
 // import MyEditor from '../components/backend/MyEditor'
@@ -14,7 +14,7 @@ const form = () => {
             <br />
             <br />
             <Form />
-            <Editor />
+            {/* <Editor /> */}
             {/* <MyEditor /> */}
         </div>
     )
